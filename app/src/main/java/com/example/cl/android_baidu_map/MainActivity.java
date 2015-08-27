1831644d6368b6696f3dd0846bbb8a746422aa69
chinadarren <chinadarren@outlook.com>
2015-08-27 18:56:53 +0800

    s

--- app/src/main/java/com/example/cl/android_baidu_map/MainActivity.java
+++ app/src/main/java/com/example/cl/android_baidu_map/MainActivity.java
@@ -1,23 +1,43 @@
 package com.example.cl.android_baidu_map;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
 
 public class MainActivity extends Activity {
 MapView mMapView ;
+    BaiduMap mBaiduMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.bmapView);
+        //利用BaiduMap中的mapType()方法来设置地图类型
+        mBaiduMap = mMapView.getMap();
+        //普通地图
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        //卫星地图
+       // mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+        //开启交通图
+        mBaiduMap.setTrafficEnabled(true);
+        //开启交通图
+        mBaiduMap.setBaiduHeatMapEnabled(true);
+
+        //定义Maker坐标点
+        LatLng Loint = new LatLng(39.963175,116.400244);
+        BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.drawable.icon_marka);
+
     }
     @Override
     protected void onDestroy() {
